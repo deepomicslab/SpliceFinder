19283f45e9aefecec7950abb8631ce7f96e7b2fc
WANG Ruohan <42761637+wangruohan111@users.noreply.github.com>
2022-04-22 00:36:40 +0800

    Update process.sh

--- iteration_process/test/process.sh
+++ iteration_process/test/process.sh
@@ -1,4 +1,4 @@
-sed -i 'samtools faidx Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa /g' dna_loc
+sed -i 's/^/samtools faidx Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa /g' dna_loc
 sh dna_loc >dna_seq
 awk 'NR%8!=1' dna_seq >dna_seq_2
 awk '{if(NR%7!=0)ORS=" ";else ORS="\n"}1' dna_seq_2 >dna_seq
